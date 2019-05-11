c54fbdf5b9954343cd14df806492d6b2aa82658c
Tomasz Falarz <t.k.falarz@gmail.com>
2019-05-11 19:28:52 +0200

    Simplifying the class, adding a new thread for listening.

--- DataAnalyzer/ConnectViaCOM.cs
+++ DataAnalyzer/ConnectViaCOM.cs
@@ -1,22 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO.Ports;
+﻿using System.IO.Ports;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace DataAnalyzer
 {
     public class ConnectViaCOM
     {
+        ThreadStart ts;
+        Thread listeningThread;
         public ConnectViaCOM(ref SerialPort serialPort)
         {
             serialPort.BaudRate = 9600;
             serialPort.PortName = "COM3";
         }
-        public void OpenSerialPort(SerialPort serialPort)
+        public void ListenPort(SerialPort serialPort)
+        {
+            OpenSerialPort(serialPort);
+            if (serialPort.IsOpen)
+            {
+                ts = new ThreadStart(delegate ()
+                {
+                    while (listeningThread.ThreadState == ThreadState.Running)
+                    {
+                        var val = serialPort.ReadLine();
+                        var dc = DataCorrection(serialPort.ReadLine());
+                        DataAggregator.AggregateData(dc.Replace("°C", ""));
+                    }
+
+                });
+                listeningThread = new Thread(ts);
+
+                if(listeningThread.ThreadState == ThreadState.Suspended)
+                    listeningThread.Resume();
+                else
+                    listeningThread.Start();
+            }
+        }
+        public void StopListeningPort(SerialPort serialPort)
+        {
+            listeningThread.Suspend();
+            serialPort.Close();
+        }
+        private string DataCorrection(string data)
+        {
+            data = data.Replace("\r", "°C");
+            data = Regex.Match(data, @"(\w{1,2}\.\w{2}°C)").ToString();
+            return data;
+        }
+        private void OpenSerialPort(SerialPort serialPort)
         {
             try
             {
@@ -24,19 +56,8 @@ namespace DataAnalyzer
             }
             catch
             {
-                MessageBox.Show("Device not available!");
+                MessageBox.Show("Device is not available!");
             }
-        }
-        public string ListenPort(SerialPort serialPort)
-        {
-            return DataCorrection(serialPort.ReadLine());
-        }
-        private string DataCorrection(string data)
-        {
-            data = data.Replace("\r", " °C");
-            data = Regex.Match(data, @"(\w{2} °C)").ToString();
-            DataAggregator.AggregateData(data.Replace(" °C", ""));
-            return data;
         }
     }
 }
