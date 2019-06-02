ad8f7fbe4b23998ecf6ea627cc1e0509064b8f08
Tomasz Falarz <t.k.falarz@gmail.com>
2019-06-02 19:20:32 +0200

    Unit tests added.

---
+++ DataAnalyzer.Tests/DataAnalyzerTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace DataAnalyzer.Tests
+{
+    public class DataAnalyzerTests
+    {
+        [Test]
+        public void DataCorrection_ShouldCorrectData()
+        {
+        }
+    }
+}
